8f0a6d2f24e7acff3e54dfe6adfbcb1ab46ffc0d
Cyprien Tertrais <cyprien.tertrais@adeo.com>
2022-10-31 13:24:31 +0100

    :tada: init forum

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Forum collab: il y a tout a créer. Principe de PR ( on tire une branche de main)</h1>
+    </div>
+  );
+}
+
+export default App;
